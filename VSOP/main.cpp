ba846fba9c5748078d4c819ec8006395d04e8e0d
Grayson <info@fromconcentratesoftware.com>
2015-09-11 13:25:09 -0700

    Initial Commit

---
+++ VSOP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VSOP
+//
+//  Created by Grayson Hansard on 9/11/15.
+//  Copyright © 2015 From Concentrate Software. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
